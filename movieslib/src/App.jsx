1de38860db97e8cf9c7733eca933d8fea8b23b1e
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-03-22 17:37:01 -0300

    finalizando funcionalidades

---
+++ movieslib/src/App.jsx
@@ -0,0 +1,25 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import Search from './pages/Search';
+import Navbar from './components/Navbar';
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="search" element={<Search />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App;
